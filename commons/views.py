c23abdd9c31a359f0a7e84261c358b2d82a484f3
Quan <daovietquan@gmail.com>
2023-06-02 15:04:31 -0400

    first commit

---
+++ commons/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to SwipeSaver!")
+
+def commons(request):
+    return HttpResponse("Welcome to Commons!")
