e38f56a643d90030adf2b2455f586af98d549fba
ishansuhail <ishansuhail28@gmail.com>
2023-07-18 15:05:47 -0400

    Updated parser, to show the menu for the day

--- commons/views.py
+++ commons/views.py
@@ -13,6 +13,22 @@ import requests
 import re
 
 
+def br_lunch_day():
+    current_date = datetime.date.today()
+    day = current_date.weekday()
+    
+    return day
+    
+
+def dinner_day():
+    current_date = datetime.date.today()
+    day = current_date.weekday()
+    if(day == 6):
+        return 0
+    day += 1
+    
+    return day
+
 def parse_html(request):
     
     
@@ -26,48 +42,85 @@ def parse_html(request):
     html_content = r.content
 
     soup = BeautifulSoup(html_content, 'html.parser')
+    br_lunch = br_lunch_day()
+    dinner = dinner_day()
     
-    
-    parse_meal(soup, 'accordion-block breakfast', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 10px; margin-left: 68px; margin-bottom: 5px">BREAKFAST (7:00 - 9:30)</p>')
-    parse_meal(soup, 'accordion-block lunch', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px;margin-bottom: 5px">LUNCH (11:00 - 3:00)</p>')
-    parse_meal(soup, 'accordion-block dinner', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">DINNER (4:30 - 8:00)</p>')
+    parse_meal(soup, 'accordion-block breakfast', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 10px; margin-left: 68px; margin-bottom: 5px">BREAKFAST (7:00 - 9:30)</p>', br_lunch)
+    parse_meal(soup, 'accordion-block lunch', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px;margin-bottom: 5px">LUNCH (11:00 - 3:00)</p>', br_lunch)
+    parse_meal(soup, 'accordion-block dinner', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">DINNER (4:30 - 8:00)</p>', dinner)
+
+    # parse_meal(soup, 'accordion-block breakfast', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 10px; margin-left: 68px; margin-bottom: 5px">BREAKFAST (7:00 - 9:30)</p>')
+    # parse_meal(soup, 'accordion-block lunch', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px;margin-bottom: 5px">LUNCH (11:00 - 3:00)</p>')
+    # parse_meal(soup, 'accordion-block dinner', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">DINNER (4:30 - 8:00)</p>')
 
     return render(request, 'commons.html')  # Render the modified template
 
-def parse_meal(soup, meal, html_pattern):
+
+def parse_meal(soup, meal, html_pattern, day):
     meal_type = soup.find_all('div', class_= meal)
 
     with open('commons/templates/commons.html', "r") as file:
         existing_html = file.read()
-
-    current_date = datetime.date.today()
-    day_ = current_date.weekday()
-    
-    count = 0
-    for i in meal_type:
         
-        meal_string = str(i)
-        pattern = r'data-fooditemname="([^"]+)"'
-        result = re.findall(pattern, meal_string)
+    meal_string = str(meal_type[day])
+    pattern = r'data-fooditemname="([^"]+)"'
+    result = re.findall(pattern, meal_string)
 
-        if result:
+    if result:
             
-            search_pattern = html_pattern + '\n<button class="accordion">Grill</button>\n<div class="panel">\n'
-            index = existing_html.find(search_pattern)
+        search_pattern = html_pattern + '\n<button class="accordion">Grill</button>\n<div class="panel">\n'
+        index = existing_html.find(search_pattern)
 
-            if index != -1:
-                # Calculate the insertion index
-                insertion_index = index + len(search_pattern)
-                modified_html = existing_html[:insertion_index]
+        if index != -1:
+            # Calculate the insertion index
+            insertion_index = index + len(search_pattern)
+            modified_html = existing_html[:insertion_index]
 
-                for item in result:
+            for item in result:
+                if item != result[-1]:
                     modified_html += f'\t\t<p style="font-size: 24px; margin-top: 25px; margin-left: 30px">{item}</p>\n\t\t<hr class="dashed-line">\n'
+                else:
+                    modified_html += f'\t\t<p style="font-size: 24px; margin-top: 25px; margin-left: 30px">{item}</p>\n\t\t<hr class="dashed-line">'
                     
-                modified_html += existing_html[insertion_index:]
+            modified_html += existing_html[insertion_index:]
 
-                with open("commons/templates/commons.html", "w") as file:
-                    file.write(modified_html)
-        count += 1
+            with open("commons/templates/commons.html", "w") as file:
+                file.write(modified_html)
+
+# def parse_meal(soup, meal, html_pattern):
+#     meal_type = soup.find_all('div', class_= meal)
+
+#     with open('commons/templates/commons.html', "r") as file:
+#         existing_html = file.read()
+
+#     current_date = datetime.date.today()
+#     day = current_date.weekday()
+    
+#     count = 0
+#     for i in meal_type:
+        
+#         meal_string = str(i)
+#         pattern = r'data-fooditemname="([^"]+)"'
+#         result = re.findall(pattern, meal_string)
+
+#         if result:
+            
+#             search_pattern = html_pattern + '\n<button class="accordion">Grill</button>\n<div class="panel">\n'
+#             index = existing_html.find(search_pattern)
+
+#             if index != -1:
+#                 # Calculate the insertion index
+#                 insertion_index = index + len(search_pattern)
+#                 modified_html = existing_html[:insertion_index]
+
+#                 for item in result:
+#                     modified_html += f'\t\t<p style="font-size: 24px; margin-top: 25px; margin-left: 30px">{item} {day_} {current_date} {len(meal_type)} </p>\n\t\t<hr class="dashed-line">\n'
+                    
+#                 modified_html += existing_html[insertion_index:]
+
+#                 with open("commons/templates/commons.html", "w") as file:
+#                     file.write(modified_html)
+#         count += 1
 
  #remove the food items
 def remove_items(request):
@@ -91,7 +144,9 @@ def remove_items(request):
         file.write(str(soup))
     
     return render(request, 'commons.html')
-    
+
+
+
     
 def home(request):
     return HttpResponse("Welcome to SwipeSaver!")
