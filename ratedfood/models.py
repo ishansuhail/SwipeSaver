17bd6fa36b5e57d27e62782b5dda9d79a7fee503
ChrisCodeOS <bobkoc@rpi.edu>
2023-07-18 15:02:22 -0400

    navbar + bootstrap for ratedfood + model(food)

--- ratedfood/models.py
+++ ratedfood/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ratedFoodItem(models.Model):
+	food_name = models.CharField(max_length=50)
+	image = models.CharField(max_length=50)
+	rating = models.DecimalField(max_digits=2, decimal_places = 1)
+	vegan = models.BooleanField()
+
+	
