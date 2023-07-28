4b1895c473ffb249c68727fe3f0d990136d592e0
ishansuhail <ishansuhail28@gmail.com>
2023-07-28 14:50:14 -0400

    Fixed the remove_items function

--- commons/views.py
+++ commons/views.py
@@ -23,139 +23,97 @@ def br_lunch_day():
 def dinner_day():
     current_date = datetime.date.today()
     day = current_date.weekday()
-    if day == 6:
+    if(day == 6):
         return 0
     day += 1
     
     return day
 
-def brunch_day():
-    current_date = datetime.date.today()
-    day = current_date.weekday()
-    
-    if day == 5:
-        return 1
-    
-    if day == 6:
-        return 0
-    
-    return -1
-        
-
 def parse_html(request):
-    
-    
+    # Open the existing HTML file
     with open('commons/templates/commons.html', 'r') as file:
         existing_html = file.read()
-    if '<hr class="dashed-line">' in existing_html:
-        return render(request, 'commons.html')
-    
-    # Perform the parsing and modification logic
-    r = requests.get("https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15465&locationId=76929001&whereami=http://rpi.sodexomyway.com/dining-near-me/commons-dining-hall")
-    html_content = r.content
 
-    soup = BeautifulSoup(html_content, 'html.parser')
-    br_lunch = br_lunch_day()
-    dinner = dinner_day()
-    #brunch = brunch()
-    
-    current_date = datetime.date.today()
-    day = current_date.weekday()
-    
-    
-    if day == 5 or day == 6:
-        # parse_meal(soup, 'accordion-block breakfast', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 10px; margin-left: 68px; margin-bottom: 5px">BREAKFAST (7:00 - 9:30)</p>', br_lunch)
-        # parse_meal(soup, 'accordion-block lunch', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px;margin-bottom: 5px">LUNCH (11:00 - 3:00)</p>', br_lunch)
-        #parse_meal(soup, 'accordion-block brunch', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">DINNER (4:30 - 8:00)</p>', brunch)
-        parse_meal(soup, 'accordion-block dinner', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">DINNER (4:30 - 8:00)</p>', dinner)
-    
-    else:
-        parse_meal(soup, 'accordion-block breakfast', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 10px; margin-left: 68px; margin-bottom: 5px">BREAKFAST (7:00 - 9:30)</p>', br_lunch)
-        parse_meal(soup, 'accordion-block lunch', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px;margin-bottom: 5px">LUNCH (11:00 - 3:00)</p>', br_lunch)
-        parse_meal(soup, 'accordion-block dinner', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">DINNER (4:30 - 8:00)</p>', dinner)
-    
+    # Check if the html file has already been parsed
+    if '<hr class="dashed-line">' not in existing_html:
+        # Perform the parsing and modification logic
+        r = requests.get("https://menus.sodexomyway.com/BiteMenu/Menu?menuId=15465&locationId=76929001&whereami=http://rpi.sodexomyway.com/dining-near-me/commons-dining-hall")
+        html_content = r.content
 
-    # parse_meal(soup, 'accordion-block breakfast', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 10px; margin-left: 68px; margin-bottom: 5px">BREAKFAST (7:00 - 9:30)</p>')
-    # parse_meal(soup, 'accordion-block lunch', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px;margin-bottom: 5px">LUNCH (11:00 - 3:00)</p>')
-    # parse_meal(soup, 'accordion-block dinner', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">DINNER (4:30 - 8:00)</p>')
+        soup = BeautifulSoup(html_content, 'html.parser')
 
-    return render(request, 'commons.html')  # Render the modified template
+        br_lunch = br_lunch_day()
+        dinner = dinner_day()
+        
+        existing_html = parse_meal(soup, 'accordion-block breakfast', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 10px; margin-left: 68px; margin-bottom: 5px">BREAKFAST (7:00 - 9:30)</p>', br_lunch, existing_html)
+        existing_html = parse_meal(soup, 'accordion-block lunch', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px;margin-bottom: 5px">LUNCH (11:00 - 3:00)</p>', br_lunch, existing_html)
+        existing_html = parse_meal(soup, 'accordion-block dinner', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">DINNER (4:30 - 8:00)</p>', dinner, existing_html)
+        
+        # Save the modified HTML
+        with open('commons/templates/commons.html', 'w') as file:
+            file.write(existing_html)
+
+    return render(request, 'commons.html')
 
 
-def parse_meal(soup, meal, html_pattern, day):
+
+def parse_meal(soup, meal, html_pattern, day, existing_html):
     meal_type = soup.find_all('div', class_= meal)
     
-
-    with open('commons/templates/commons.html', "r") as file:
-        existing_html = file.read()
-        
     meal_string = str(meal_type[day])
     
-    pattern = r'data-fooditemname="([^"]+)"'
-    calorie_pattern = r'(\d+cal)'
+    meal_soup = BeautifulSoup(meal_string, 'html.parser')
     
-    result = re.findall(pattern, meal_string)
+    courses = meal_soup.find_all('div', class_='bite-menu-course')
     
-    calorie_result = re.findall(calorie_pattern, meal_string)
-    # calorie_result = calorie_result[1::2]
+    search_pattern = html_pattern
+    index = existing_html.find(search_pattern)
 
-    if result:
-            
-        search_pattern = html_pattern + '\n<button class="accordion">Grill</button>\n<div class="panel">\n'
-        index = existing_html.find(search_pattern)
-
-        if index != -1:
-            # Calculate the insertion index
-            insertion_index = index + len(search_pattern)
-            modified_html = existing_html[:insertion_index]
-
-            for item1, item2 in zip(result, calorie_result):
-                if item1 == 'Have A Nice Day!':
-                    continue
-                if item1 != result[-1]:
-                    modified_html += f'\t\t<p style="font-size: 24px; margin-top: 25px; margin-left: 30px">{item1} {item2}</p>\n\t\t<hr class="dashed-line">\n'
-                else:
-                    modified_html += f'\t\t<p style="font-size: 24px; margin-top: 25px; margin-left: 30px">{item1} {item2}</p>\n\t\t<hr class="dashed-line">'
-                    
-            modified_html += existing_html[insertion_index:]
-
-            with open("commons/templates/commons.html", "w") as file:
-                file.write(modified_html)
-
-# def parse_meal(soup, meal, html_pattern):
-#     meal_type = soup.find_all('div', class_= meal)
-
-#     with open('commons/templates/commons.html', "r") as file:
-#         existing_html = file.read()
-
-#     current_date = datetime.date.today()
-#     day = current_date.weekday()
-    
-#     count = 0
-#     for i in meal_type:
+    if index != -1:
+        # Calculate the insertion index
+        insertion_index = index + len(search_pattern)
+        modified_html = existing_html[:insertion_index]
         
-#         meal_string = str(i)
-#         pattern = r'data-fooditemname="([^"]+)"'
-#         result = re.findall(pattern, meal_string)
+        
 
-#         if result:
+        for course in courses:
+            station_name = course.find('h5').get_text(strip=True)
+            modified_html += f'\n<button class="accordion">{station_name}</button>\n<div class="panel">\n'
             
-#             search_pattern = html_pattern + '\n<button class="accordion">Grill</button>\n<div class="panel">\n'
-#             index = existing_html.find(search_pattern)
+            items = course.find_next_sibling('ul').find_all('li')
+                     
+            for item in items:
+                
+                item_name = re.search(r'data-fooditemname="([^"]+)"', str(item))
+                
+                if item_name:
+                    item_name = item_name.group(1)
+                    modified_html += f'\t\t<p style="font-size: 20px; margin-top: 20px; margin-left: 35px">{item_name} </p>\n\t\t<hr class="dashed-line">\n'
 
-#             if index != -1:
-#                 # Calculate the insertion index
-#                 insertion_index = index + len(search_pattern)
-#                 modified_html = existing_html[:insertion_index]
+            # Add rating system for the station
+            station_name_slug = station_name.lower().replace(" ", "-")  # convert station name to lower case and replace spaces with hyphens
+            rating_html = f'''
+<p style="color: rgb(228, 30, 30); font-size: 19px; margin-top: 25px; margin-bottom: 10px; margin-right: 33px; text-align: right">RATE THIS STATION</p>
+<div class="rating">
+<input id="star5-{meal}-{station_name_slug}" name="rating-{meal}-{station_name_slug}" type="radio" value="5"/><label for="star5-{meal}-{station_name_slug}" title="Rocks!">5 stars</label>
+<input id="star4-{meal}-{station_name_slug}" name="rating-{meal}-{station_name_slug}" type="radio" value="4"/><label for="star4-{meal}-{station_name_slug}" title="Pretty good">4 stars</label>
+<input id="star3-{meal}-{station_name_slug}" name="rating-{meal}-{station_name_slug}" type="radio" value="3"/><label for="star3-{meal}-{station_name_slug}" title="Meh">3 stars</label>
+<input id="star2-{meal}-{station_name_slug}" name="rating-{meal}-{station_name_slug}" type="radio" value="2"/><label for="star2-{meal}-{station_name_slug}" title="Kinda bad">2 stars</label>
+<input id="star1-{meal}-{station_name_slug}" name="rating-{meal}-{station_name_slug}" type="radio" value="1"/><label for="star1-{meal}-{station_name_slug}" title="Sucks big time">1 star</label>
+</div>
+'''
+            modified_html += rating_html
 
-#                 for item in result:
-#                     modified_html += f'\t\t<p style="font-size: 24px; margin-top: 25px; margin-left: 30px">{item} {day_} {current_date} {len(meal_type)} </p>\n\t\t<hr class="dashed-line">\n'
-                    
-#                 modified_html += existing_html[insertion_index:]
+            modified_html += '\t\t</div>\n'
+            
+        modified_html += existing_html[insertion_index:]
 
-#                 with open("commons/templates/commons.html", "w") as file:
-#                     file.write(modified_html)
-#         count += 1
+        return modified_html
+
+    else:
+        # If the search_pattern is not found, return the existing_html as is
+        return existing_html
+
 
  #remove the food items
 def remove_items(request):
@@ -163,6 +121,21 @@ def remove_items(request):
         file_change = file.read()
     
     soup = BeautifulSoup(file_change, 'html.parser')
+    
+    headers = soup.find_all('p', style = 'color: rgb(228, 30, 30); font-size: 19px; margin-top: 25px; margin-bottom: 10px; margin-right: 33px; text-align: right')
+    
+    for header in headers:
+        header.extract()
+        
+    buttons = soup.find_all('button', class_ = 'accordion')
+    
+    for button in buttons:
+        button.extract()
+        
+    panels = soup.find_all('div', class_ = 'panel')
+    
+    for panel in panels:
+        panel.extract()
     
     elements = soup.find_all('p', style = 'font-size: 24px; margin-top: 25px; margin-left: 30px')
     
@@ -173,7 +146,8 @@ def remove_items(request):
     
     for element in second_elements:
         element.extract()
-    
+        
+
     
     with open('commons/templates/commons.html', 'w') as file:
         file.write(str(soup))
@@ -189,8 +163,11 @@ def home(request):
 def commons(request):
     return HttpResponse("Welcome to Commons!")
 
+def is_ajax(request):
+    return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
+
 def rate(request):
-    if request.is_ajax():
+    if is_ajax(request):
         rating_val = None
         station_name = None
         meal_time = None
@@ -214,4 +191,3 @@ def rate(request):
 
             return JsonResponse({'success': True, 'average_rating': average_rating})
     return JsonResponse({'success': False})
-
