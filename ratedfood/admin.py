6f638d335b91f6f380bae9b5e5daaefd519ab0e8
ChrisCodeOS <bobkoc@rpi.edu>
2023-10-15 21:45:23 -0400

    changes to ratedfood

--- ratedfood/admin.py
+++ ratedfood/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import ratedFoodItem
+
+class RatedAdmin(admin.ModelAdmin):
+	list_display = ('food_name','image','rating','vegan','dining_hall' )
+admin.site.register(ratedFoodItem,RatedAdmin)
--- ratedfood/models.py
+++ ratedfood/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class ratedFoodItem(models.Model):
+	food_name = models.CharField(max_length=50)
+	image = models.ImageField(upload_to='ratedfood_images/', blank=True, null=True)
+	rating = models.DecimalField(max_digits=2, decimal_places = 1)
+	vegan = models.BooleanField()
+	dining_hall = models.CharField(max_length=50, default = 'commons')
+
+	
