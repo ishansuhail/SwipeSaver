124578f32e28a1fe010890317778d10c6dbb751b
ChrisCodeOS <bobkoc@rpi.edu>
2023-10-15 21:38:17 -0400

    Changes to contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import contactForm
+from .models import Survey
+
+class ContactAdmin(admin.ModelAdmin):
+	list_display = ('email','name','title','description')
+admin.site.register(contactForm,ContactAdmin)
+admin.site.register(Survey)
 # Register your models here.
