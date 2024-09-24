11f31da2e86e06e0e3cdd92187ed66deec3705e5
ishansuhail <ishansuhail28@gmail.com>
2024-09-23 21:41:18 -0400

    fetch the json data from the site

--- commons/views.py
+++ commons/views.py
@@ -11,230 +11,63 @@ from django.http import HttpResponse
 from bs4 import BeautifulSoup
 import requests
 import re
+import json
 from unidecode import unidecode
 
 
-def br_lunch_day():
-    current_date = datetime.date.today()
-    day = current_date.weekday()
-    #day = 5
-    return day
-    
-
-def dinner_day():
-    current_date = datetime.date.today()
-    day = current_date.weekday()
-    #day = 5
-    if(day == 6):
-        return 0
-    day += 1
-    
-    return day
-
-def parse_html(request):
-    # Open the existing HTML file
-    with open('commons/templates/commons.html', 'r') as file:
-        existing_html = file.read()
-
-    # Check if the html file has already been parsed
-    if '<hr class="dashed-line">' not in existing_html:
-        # Perform the parsing and modification logic
-        r = requests.get("https://rpi.sodexomyway.com/en-us/locations/the-commons-dining-hall")
-
-        print(r.status_code)
-    
-        if r.status_code != 200:
-            return HttpResponse("Error fetching the menu")
-        
-
-        html_content = r.content
-
-        soup = BeautifulSoup(html_content, 'html.parser')
-
-        br_lunch = br_lunch_day()
-        dinner = dinner_day()
-        current_date = datetime.date.today()
-        day = current_date.weekday()
-        
-        
-        if day >= 5:
-            
-            print("reached")
-            soup2 = BeautifulSoup(existing_html, 'html.parser')
-        
-            target_elements = soup2.find_all('p', style = 'color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px')
-            print(target_elements)
-            breakfast_tag = "BREAKFAST (7:00 - 9:30)"
-
-            for tag in target_elements:
-                if tag.text == breakfast_tag:
-                    new_content = "BRUNCH (7:00 - 9:30)"  # Replace with your desired content
-                    tag.string.replace_with(new_content)
-
-            with open('commons/templates/commons.html', 'w') as file:
-                file.write(str(soup2))
-            
-            with open('commons/templates/commons.html', 'r') as file:
-                existing_html = file.read()
-            
-            brunch_day = 0
-            
-            if day == 6:
-                brunch_day = 1
-            
-            existing_html = parse_meal(soup, 'accordion-block brunch', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">BRUNCH (7:00 - 9:30)</p>', brunch_day, existing_html)
-            existing_html = parse_meal(soup, 'accordion-block dinner', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">DINNER (4:30 - 8:00)</p>', br_lunch, existing_html)
-            
-                
-                    
-        if day < 5:
-            
-            soup2 = BeautifulSoup(existing_html, 'html.parser')
-        
-            target_elements = soup2.find_all('p', style = 'color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px')
-            print("target_elements", target_elements)
-            breakfast_tag = "BRUNCH (7:00 - 9:30)"
-
-            for tag in target_elements:
-                if tag.text == breakfast_tag:
-                    new_content = "BREAKFAST (7:00 - 9:30)"  # Replace with your desired content
-                    tag.string.replace_with(new_content)
-
-            with open('commons/templates/commons.html', 'w') as file:
-                file.write(str(soup2))
-            
-            with open('commons/templates/commons.html', 'r') as file:
-                existing_html = file.read()
-                
-            existing_html = parse_meal(soup, 'accordion-block breakfast', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">BREAKFAST (7:00 - 9:30)</p>', br_lunch, existing_html)
-            existing_html = parse_meal(soup, 'accordion-block lunch', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">LUNCH (11:00 - 3:00)</p>', br_lunch, existing_html)
-            existing_html = parse_meal(soup, 'accordion-block dinner', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 35px; margin-left: 68px; margin-bottom: 5px">DINNER (4:30 - 8:00)</p>', dinner, existing_html)
-            
-
-        with open('commons/templates/commons.html', 'w') as file:
-            file.write(existing_html)
-            # Save the modified HTML
-            
-        # else:
-        #     existing_html = parse_meal(soup, 'accordion-block breakfast', '<p style="color: rgb(228, 30, 30); font-size: 32px; margin-top: 10px; margin-left: 68px; margin-bottom: 5px">BRUNCH (7:00 - 9:30)</p>', br_lunch, existing_html)
-            
-            
-        
-                
-    return render(request, 'commons.html')
-
-
-
-def parse_meal(soup, meal, html_pattern, day, existing_html):
-    meal = meal.split(' ')[1]
-    meal_type = soup.find_all('div', class_= meal)
-    #meal_string = str(meal_type[day])
-    
-    if len(meal_type) > day:
-        meal_string = str(meal_type[day])
-    else:
-        return existing_html
-    
-    meal_soup = BeautifulSoup(meal_string, 'html.parser')
-    
-    courses = meal_soup.find_all('div', class_='bite-menu-course')
-    
-    search_pattern = html_pattern
-    index = existing_html.find(search_pattern)
-
-    if index != -1:
-        # Calculate the insertion index
-        insertion_index = index + len(search_pattern)
-        modified_html = existing_html[:insertion_index]
-
-        for course in courses:
-            station_name = course.find('h5').get_text(strip=True)
-            modified_html += f'\n<button class="accordion">{station_name}</button>\n<div class="panel">\n'
-            
-            items = course.find_next_sibling('ul').find_all('li')
-            
-            for item in items:
-                item_renamed = unidecode(str(item))
-                item_name = re.search(r'data-fooditemname="([^"]+)"', item_renamed)
-                
-                calories = re.search(r'\d+cal', str(item))
-                                     
-                if item_name and calories:
-                    item_name = item_name.group(1)
-                    calories_group = calories.group()
-                    match = re.search(r'\d+', calories_group)
-                    cal = int(match.group())
-                    
-                    modified_html += f'\t\t<p style="font-size: 20px; margin-top: 20px; margin-left: 35px">{item_name} <button onclick = "addCalories({cal})" class = "button" data-calories = "{cal}" style = "color: red; border: none; font-size: 20px;">{calories.group()} </button></p>\n\t\t<hr class="dashed-line">\n'
-            # Add rating system for the station
-            station_name_slug = station_name.lower().replace(" ", "-")  # convert station name to lower case and replace spaces with hyphens
-            rating_html = f'''
-<p style="color: rgb(228, 30, 30); font-size: 19px; margin-top: 25px; margin-bottom: 10px; margin-right: 33px; text-align: right">RATE THIS STATION</p>
-<div class="rating">
-<input id="star5-{meal}-{station_name_slug}" name="rating-{meal}-{station_name_slug}" type="radio" value="5"/><label for="star5-{meal}-{station_name_slug}" title="Rocks!">5 stars</label>
-<input id="star4-{meal}-{station_name_slug}" name="rating-{meal}-{station_name_slug}" type="radio" value="4"/><label for="star4-{meal}-{station_name_slug}" title="Pretty good">4 stars</label>
-<input id="star3-{meal}-{station_name_slug}" name="rating-{meal}-{station_name_slug}" type="radio" value="3"/><label for="star3-{meal}-{station_name_slug}" title="Meh">3 stars</label>
-<input id="star2-{meal}-{station_name_slug}" name="rating-{meal}-{station_name_slug}" type="radio" value="2"/><label for="star2-{meal}-{station_name_slug}" title="Kinda bad">2 stars</label>
-<input id="star1-{meal}-{station_name_slug}" name="rating-{meal}-{station_name_slug}" type="radio" value="1"/><label for="star1-{meal}-{station_name_slug}" title="Sucks big time">1 star</label>
-</div>
-'''
-            modified_html += rating_html
-
-            modified_html += '\t\t</div>\n'
-            
-        modified_html += existing_html[insertion_index:]
-
-        return modified_html
-
-    else:
-        # If the search_pattern is not found, return the existing_html as is
-        return existing_html
-
-
- #remove the food items
-def remove_items(request):
-    
-    with open('commons/templates/commons.html', 'r') as file:
-        file_change = file.read()
-    
-    soup = BeautifulSoup(file_change, 'html.parser')
-    
-    headers = soup.find_all('p', style = 'color: rgb(228, 30, 30); font-size: 19px; margin-top: 25px; margin-bottom: 10px; margin-right: 33px; text-align: right')
-
-    for header in headers:
-        header.extract()
-
-    buttons = soup.find_all('button', class_ = 'accordion')
-
-    for button in buttons:
-        button.extract()
-
-    panels = soup.find_all('div', class_ = 'panel')
-
-    for panel in panels:
-        panel.extract()
-
-    elements = soup.find_all('p', style = 'font-size: 24px; margin-top: 25px; margin-left: 30px')
-
-    for element in elements:
-        element.extract()
-    
-    second_elements = soup.find_all('hr', class_ = 'dashed-line')
-
-    for element in second_elements:
-        element.extract()
-        
-    with open('commons/templates/commons.html', 'w') as file:
-        file.write(str(soup))
-
-    return render(request, 'commons.html')
-
-    
-def home(request):
-    return HttpResponse("Welcome to SwipeSaver!")
 
 def commons(request):
-    return render(request, 'commons/commons.html')
+    populate()
+    return render(request, 'commons.html')
+
+
+def populate(url = "https://rpi.sodexomyway.com/en-us/locations/the-commons-dining-hall"):
+    response = requests.get(url)
+    html_content = response.text
+    
+    soup = BeautifulSoup(html_content, 'html.parser')
+    script_tag = soup.find('script', text=re.compile(r'window\.__PRELOADED_STATE__'))
+    if script_tag:
+        script_content = script_tag.string
+
+        # print(script_content)
+        split_content = script_content.split('window.__PRELOADED_STATE__ = ')
+        
+        if split_content:
+        # Print the parts before and after the split
+        # print("Part before 'window.__PRELOADED_STATE__':")
+        # print(split_content[0].strip())  # Part before the delimiter
+        # print("\nPart after 'window.__PRELOADED_STATE__':")
+            try: 
+                json_data = json.loads(split_content[1].strip())  # Parse the JSON string into a Python dictionary
+                file_name = 'output.txt'  # You can change this to any file name or path
+
+# Open the file in write mode and save the content
+                with open(file_name, 'w', encoding='utf-8') as file:
+                    data = json_data.get('composition').get('subject').get('regions')[1].get('fragments')[0].get('content').get('main').get('sections')
+                    print(data[0].get('name'))
+                    print(data[1].get('name'))
+                    print(data[2].get('name'))
+                    file.write(json.dumps(data, indent= 2))
+                # Get the ratings data
+            except json.JSONDecodeError as e:
+                print("Error decoding JSON:", e)  # Part after the delimiter
+        
+        # if s:
+        #     json_data_str = json_data_match.group(1)  # Extract the matched JSON string
+            
+        #     try:
+        #         # Step 3: Parse the JSON string into a Python dictionary
+        #         json_data = json.loads(json_data_str)
+                
+        #         # Print the extracted JSON data
+        #         print(json.dumps(json_data, indent=4))  # Pretty print the JSON data
+        #     except json.JSONDecodeError as e:
+        #         print("Error decoding JSON:", e)
+        # else:
+        #     print("No JSON data found.")
+    
+
 
 def is_ajax(request):
     return request.META.get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest'
--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from commons.views import parse_html as commons_parse_html, remove_items as commons_remove_items
+from commons.views import commons as commons_view
 from barh.views import parse_html as barh_parse_html, remove_items as barh_remove_items
 from russellsage.views import parse_html as russellsage_parse_html, remove_items as russellsage_remove_items
 from blitman.views import parse_html as blitman_parse_html, remove_items as blitman_remove_items
@@ -8,8 +8,7 @@ def home(request):
     return render(request, 'homepage.html')
 
 def commons(request):
-    commons_remove_items(request)
-    return commons_parse_html(request)
+     return commons_view(request)
 
 def barh(request):
     barh_remove_items(request)
