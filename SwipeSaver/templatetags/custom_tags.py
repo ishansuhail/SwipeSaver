d5b16aa3689b90bdffa7e1104859d89a491ca241
oek0828 <oek0828@gmail.com>
2024-10-15 17:16:27 -0400

    Updating UI Styling of Dining Hall Pages

--- SwipeSaver/templatetags/custom_tags.py
+++ SwipeSaver/templatetags/custom_tags.py
@@ -5,16 +5,24 @@ register = template.Library()
 
 @register.simple_tag
 def render_meal_info(meal_items):
-    html_parts = []  # Use a list to collect parts
+    html_parts = []
     prev_station = ""
+    meal_count = 0
     
     for item in meal_items:
         if str(item.station) != "None":
             if item.station != prev_station:
+                # Close the previous accordion if there was one
+                if prev_station:
+                    html_parts.append('</div>')  # Close previous panel div
+                
+                # Add new accordion button and panel
                 html_parts.append(f'''
-                    <p class="station-style">{item.station}</p>         
+                    <button class="accordion">{item.station}</button>
+                    <div class="panel">
                 ''')
                 prev_station = item.station
+                meal_count = 0  # Reset meal count for the new station
 
             description = item.description if item.description != "" else "No description available"
             html_parts.append(f'''
@@ -28,5 +36,14 @@ def render_meal_info(meal_items):
                 </div>
             </div>
             ''')
+            
+            # Add red dotted line after each item except the last one
+            meal_count += 1
+            if meal_count < len(meal_items):
+                html_parts.append('<hr class="dashed-line">')
+    
+    # Close the last panel if there are items
+    if prev_station:
+        html_parts.append('</div>')
     
     return mark_safe(''.join(html_parts))
