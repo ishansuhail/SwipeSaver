0a538a37f890201bb12dc6acad31ba34fa132499
oek0828 <oek0828@gmail.com>
2024-10-22 17:28:18 -0400

    Final Updates Russell Sage data

--- homepage/views.py
+++ homepage/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from commons.views import commons as commons_view
 from barh.views import parse_html as barh_parse_html, remove_items as barh_remove_items
-from russellsage.views import parse_html as russellsage_parse_html, remove_items as russellsage_remove_items
+from russellsage.views import russellsage as russellsage_view
 from blitman.views import parse_html as blitman_parse_html, remove_items as blitman_remove_items
+
 
 def home(request):
     return render(request, 'homepage.html')
@@ -15,8 +16,7 @@ def barh(request):
     return barh_parse_html(request)
 
 def russellsage(request):
-    russellsage_remove_items(request)
-    return russellsage_parse_html(request)
+    return russellsage_view(request)
 
 def blitman(request):
     blitman_remove_items(request)
