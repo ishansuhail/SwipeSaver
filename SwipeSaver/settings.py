30efb2e6a279d7561f3a3d207dd049e2e70046c9
ishansuhail <ishansuhail28@gmail.com>
2024-10-25 16:26:55 -0400

    turned on allowed hosst and added

--- SwipeSaver/settings.py
+++ SwipeSaver/settings.py
@@ -26,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-owhocqew%b896oy1b4bnfhn^qpyg(p#g!6ih@vv53%9na55=!y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['swipesaver.cs.rpi.edu']
 
 
 # Application definition
