291006507e478b7bdbd13eedba89528fc3cc8c9c
Aiden Drover-Mattinen <adrovermattinen@gmail.com>
2024-10-25 22:37:47 -0400

    Adding routing for web socket

--- commons/urls.py
+++ commons/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from . import consumers
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.commons, name='commons'),
     path('submit-rating/', views.submit_rating, name='submit_rating'),
+]
+
+websocket_urlpatterns = [
+    path("ws/log/", consumers.LogConsumer.as_asgi()),
 ]
