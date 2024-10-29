d1ab1d74db009a1f2d63fdf6e1e23ea271650b42
oek0828 <oek0828@gmail.com>
2024-10-29 16:15:38 -0400

    adding databbase to barh dining hall
    Co-authored-by: Quan <daoq@rpi.edu>

--- barh/urls.py
+++ barh/urls.py
@@ -3,8 +3,6 @@ from . import views
 
 urlpatterns = [
 
-    path('parse-html/', views.parse_html, name='parse_html'),
-    path('remove_items', views.remove_items, name='remove_items'),
     path('', views.barh, name='barh'),
     path('rate/', views.rate, name='rate'),
 
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from commons.views import commons as commons_view
-from barh.views import parse_html as barh_parse_html, remove_items as barh_remove_items
+from barh.views import barh as barh_view
 from russellsage.views import russellsage as russellsage_view
 from blitman.views import blitman as blitman_view
 
@@ -11,8 +11,7 @@ def commons(request):
      return commons_view(request)
 
 def barh(request):
-    barh_remove_items(request)
-    return barh_parse_html(request)
+    return barh_view(request)
 
 def russellsage(request):
     return russellsage_view(request)
