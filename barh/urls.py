7dfad3e15ffd1605eab4758ad442a77fe67199ad
Aiden Drover-Mattinen <adrovermattinen@gmail.com>
2024-11-08 16:04:02 -0500

    Adding functionality for barh

--- barh/urls.py
+++ barh/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 from . import views
+from . import consumers
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-
     path('', views.barh, name='barh'),
-    path('rate/', views.rate, name='rate'),
+    path('submit-rating/', views.submit_rating, name='submit_rating'),
+]
 
+barh_websocket_urlpatterns = [
+    path("ws/log/barh/", consumers.LogConsumer.as_asgi()),
 ]
